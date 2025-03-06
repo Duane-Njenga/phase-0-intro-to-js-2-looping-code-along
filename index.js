7c861daeba2ae86e17fb00ca55cc06cb4c26db2a
Dwayne <dune.njenga@student.moringaschool.com>
2025-03-06 16:57:00 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(name, type){
+    let cards = []
+        for (let i = 0; i < name.length; i++) {
+            cards.push(`Thank you, ${name[i]}, for the wonderful ${type} gift!`);
+        }
+    return cards
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number-- ;
+    }
+
+}
+
+console.log(countdown(10));
+
